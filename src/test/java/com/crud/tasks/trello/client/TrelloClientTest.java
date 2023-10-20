ee898adbe4de27b66cbac7771374873426698269
DamianNu <damian21v@gmail.com>
2023-10-20 17:55:19 +0200

    Task 22.4 ' piszemy test '

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -88,11 +88,17 @@ class TrelloClientTest {
     @Test
     public void shouldReturnEmptyList() throws URISyntaxException {
         // Given
+        when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
+        when(trelloConfig.getTrelloAppKey()).thenReturn("test");
+        when(trelloConfig.getTrelloToken()).thenReturn("test");
+        when(trelloConfig.getTrelloUser()).thenReturn("test");
 
+        URI uri = new URI("http://test.com/members/test/boards?key=test&token=test&fields=name,id&lists=all");
+
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(null);
         // When
-
+        List<TrelloBoardDto> fetchedTrelloBoardsNull = trelloClient.getTrelloBoards();
         // Then
-
+        assertEquals(0, fetchedTrelloBoardsNull.size());
     }
-
 }
