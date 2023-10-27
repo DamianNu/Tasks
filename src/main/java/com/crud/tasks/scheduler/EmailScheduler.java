858f1d90a5a013509aff1734d84c4af76dcd3686
DamianNu <damian21v@gmail.com>
2023-10-27 21:11:42 +0200

    korekta zadania 23.4

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -17,9 +17,7 @@ public class EmailScheduler {
     private final TaskRepository taskRepository;
     private final AdminConfig adminConfig;
 
-    //    @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
-
+    @Scheduled(cron = "0 0 10 * * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String a;
